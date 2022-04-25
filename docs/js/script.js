df05def932516b16dc7ad816c76f65562794a9ce
black112301 <01eramirez01@gmail.com>
2022-04-25 16:34:14 -0700

    testing for safari

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,3 +4,12 @@ var contact = document.querySelector("#contact");
 contactLink.addEventListener("click", () => {
     contact.scrollIntoView({behavior: "smooth"});
 }) 
+
+// $('body').on('click touchstart', () => {
+//     const videoElement = document.querySelectorAll('content-box');
+//     if (videoElement.playing) {
+//     }
+//     else {
+//         videoElement.play();
+//     }
+// });
