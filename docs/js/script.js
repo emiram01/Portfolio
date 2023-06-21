aacfd4472527f0dd89c853ac872f76cc4a3191ac
black112301 <01eramirez01@gmail.com>
2023-06-20 23:48:13 -0700

    refactored the styling, updated resume

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,6 @@
-var contactLink = document.querySelector("#contact-link");
-var contact = document.querySelector("#contact");
+const contactLink = document.querySelector("#contact-link");
+const contact = document.querySelector("#contact");
 
 contactLink.addEventListener("click", () => {
     contact.scrollIntoView({behavior: "smooth"});
 }) 
-
-// $('body').on('click touchstart', () => {
-//     const videoElement = document.querySelectorAll('content-box');
-//     if (videoElement.playing) {
-//     }
-//     else {
-//         videoElement.play();
-//     }
-// });
