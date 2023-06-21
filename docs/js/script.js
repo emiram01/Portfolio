a64d992934cd8bf7d7724cc96e7e295800b3b961
black112301 <01eramirez01@gmail.com>
2023-06-21 03:42:08 -0700

    fixed up some styling

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,29 @@
-const contactLink = document.querySelector("#contact-link");
+const contactBtn = document.querySelector("#contact-button");
 const contact = document.querySelector("#contact");
+const emailBtn = document.querySelector("#email-button");
 
-contactLink.addEventListener("click", () => {
+contactBtn.addEventListener("click", () => {
     contact.scrollIntoView({behavior: "smooth"});
 }) 
+
+emailBtn.addEventListener("click", () => {
+    copyToClipboard("01eramirez01@gmail.com");
+})
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        showPopup();
+    })
+    .catch((error) => {
+        console.error("Failed to copy:", error);
+    });
+}
+
+function showPopup() {
+    const popup = document.getElementById("popup");
+    popup.style.display = "block";
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 2000);
+}
